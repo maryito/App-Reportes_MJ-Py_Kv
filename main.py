bf1501ecb7988d038cef614998d3d8ae1502c56d
maryito <m_torres@uip.edu.pa>
2015-09-28 17:17:31 -0500

    Terminando todos los modulos requeridos a ala espera de diseño y la obtencion de datos tambien la validacion a traves de los popup

--- main.py
+++ main.py
@@ -7,55 +7,103 @@ from kivy.uix.screenmanager import ScreenManager,Screen
 import time
 from kivy.uix.label import Label
 from tool  import fechas
-
-#me quede sobre pensando en cargar los 
+import random
+ 
+"""
+	POr investigar
+	como usar los Popup para usarlo validar campos
+	como ponemos visualizar en formade tabla los datos a imprimir y consultar
+	buscar como hacer para enviar los datos  procesados
+""" 
 
 class User(ScreenManager):
 	nombre = ObjectProperty()
 	empresa = ObjectProperty()
 	calendario = fechas()
 
-	def horario(self):
-		self.fecha= self.calendario[0]
-		self.hora= self.calendario[1]
-	def categoria(self,consulta, screen):
-		self.var_consul = consulta
-		self.name = screen	
-
-	def usuario(self):
-		name = (self.nombre.text).strip()
-		empresa = (self.empresa.text).strip()
-		
-		if (name != "" and empresa != ""):
-			print("Bien")
-			# self.status =""
-			# print("Usuario: "+name, "Ventana: "+self.status)
-		elif name == "" and empresa != "":
-			print("Falta Nombre")
-		elif empresa == "" and name != "":
-			print("Falta Empresa")
-		else:
-			print("Falta")
-			# self.status = "usu"
-			# print("Usuario: "+name, "Ventana: "+self.status)
 	#necesitamos POpup para confirmacion y validacion de datos	
-	espera = ["hoaass","111111",4444]
+	
 	div=	ObjectProperty()
 	cat=	ObjectProperty()
 	subcat=	ObjectProperty()
 	med=	ObjectProperty()
 	marca=	ObjectProperty()
 	sku=	ObjectProperty()
-	search_results_list= ObjectProperty()
+	resultado_lista= ObjectProperty()
+	
+	espera = []
+	add ={}
+	temp =[]
+	sav = False 
+	fin = True
+	def iniciar(self):
+		self.fecha= self.calendario[0]
+		self.hora= self.calendario[1]	
+		self.resultado_lista.item_strings = ["NO HAY DATOS PROCESADOS ACTUALMENTE"]	
+		print("Inicando...")	
+	def categoria(self,consulta, screen):
+		self.var_consul = consulta
+		self.name = screen
+		print("Consultando...")	
+	def limpiar(self,):
+		self.div.text=self.cat.text=self.subcat.text=self.med.text=self.marca.text =self.sku.text=""
+		temp= espera = []
+		print("-Limpiando")
+	def usuario(self):
+		name = (self.nombre.text).strip()
+		empresa = (self.empresa.text).strip()
+		estado = (self.empresa.text).strip()
+		#usu = name+empresa+fecha+hora
+		if (name != "" and empresa != ""): estado="usuario--Bien"; print(estado)
+		elif name == "" and empresa != "": estado="usuario--Falta Nombre";print("Falta Nombre")
+		elif empresa == "" and name != "": estado="usuario--Falta Empresa";print("Falta Empresa")
+		else: print("usuario-- Falta usuario")
+	
 	def save_new_sku(self): #funcion nos servira para guardar la data ingresada
-		self.espera = [self.div.text,self.cat.text,self.subcat.text,self.med.text,self.marca.text , self.sku.text]
-		
+		#asignamos los valores temporablemente a una lista
+		temp =[self.div.text,self.cat.text,self.subcat.text,self.med.text,self.marca.text , self.sku.text]
+		self.espera =[]
+		self.espera = temp
+		#agragamos a la lista los valores ingresados		
 		if "" in self.espera:
-			print("Datos Vacios o Incorrectos")
-		else:			
-			print(self.espera)
-			print("Datos Correctos")
-			self.search_results_list.item_strings = self.espera
+			print("save_new_sku-- Datos Vacios o Incorrectos")
+		else:
+			self.sav = True			
+			print(self.temp)
+			print("save_new_sku-- Datos Correctos")
+			self.fin = False
+	def save(self): #funcion nos servira para guardar la data ingresada
+		
+		# self.espera =[]
+		temp =[self.div.text,self.cat.text,self.subcat.text,self.med.text,self.marca.text , self.sku.text]
+		
+		#agragamos a la lista los valores ingresados
+		if self.fin == False:
+			for x in temp:
+		 		self.espera.append(x)
+		 	print(self.espera)
+		 	print("save-- Datos Correctos")
+		else:
+			print("save-- Datos Vacios o Incorrectos")
+		# for x in temp:
+		# 	self.espera.append(x)
+		# if "" in self.espera:
+		# 	print("save-- Datos Vacios o Incorrectos")
+		# else:
+		# 	#clave id  y los valores serial temp
+						
+		# 	print(self.add)
+		# 	print("save-- Datos Correctos")
+	def save_db(self,):
+		if self.sav == False:
+			print("save_db-- No Procesados base da datos")
+		else:	
+			id = random.getrandbits(10)
+			self.add[id]=self.espera
+			self.resultado_lista.item_strings = ["{0}, {1}".format(clave,valor) for clave,valor in self.add.items()]
+			print(self.add)
+			print("save_db--Actualizar base da datos")		
+
 class ReportesApp(App):
 	title = 'reportes'
 	icon = 'icon.png'
@@ -64,6 +112,8 @@ class ReportesApp(App):
 		return User()
 	def on_pause(self):
 		return True
+
+
 
 if __name__ == '__main__':
 	ReportesApp().run()
